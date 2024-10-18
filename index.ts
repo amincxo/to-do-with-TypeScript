7f75185bfd0250ffa0fc6cf0b338259c298ddea7
unknown <amincxo@gmail.com>
2024-10-19 02:44:14 +0330

    create todo complate

--- index.ts
+++ index.ts
@@ -1,12 +1,55 @@
 
+enum State {
+    QUEUE="queue",
+    DONE="done",
+    CANCEL="cancel",
+    TOP_LIST="topList"
+}
 
-class Todo {
+type Todo = {
+    id:number;
+    title:string;
+    state:State
+}
+
+type createTodoDTO = {
+    title: string;
+    state : State;
+}
+
+interface ITodo {
+    createTodo(todo: createTodoDTO): void
+    delete(id: number ): void;
+    getList(): void
+    getByID(id: number) : void;
+}
+
+abstract class TodoRepository {
+    protected Todos: Todo[];
+    constructor(){
+        this.Todos = []
+    }
+    public createTodo(todo: createTodoDTO): void {
+
+    }
+    public delete(id: number ): void {
+
+    }
+    public getList(): void {    }
+    public getByID(id: number) : void {   }
+}
+
+class TodoController extends TodoRepository {
     protected Todo: object [];
     constructor () {
-        this.Todo = [];
+        super()
     }
-    new():void{}
-    delete():void{}
-    getAll(): any[]{return[]}
-    getById(): undefined |object {return}
+    public createTodo(todo: createTodoDTO): any {
+        return {newTodo: "created"}
+    } 
 }
+
+const todo = new TodoController();
+
+console.log(todo.createTodo({title : " ", state: State.DONE }))
+
