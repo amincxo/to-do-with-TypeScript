667f20c7bbdbe6f471f6906572c5cecebe25f212
unknown <amincxo@gmail.com>
2024-10-19 01:51:20 +0330

    inisalise

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+
+class Todo {
+    protected Todo: object [];
+    constructor () {
+        this.Todo = [];
+    }
+    new():void{}
+    delete():void{}
+    getAll(): any[]{return[]}
+    getById(): undefined |object {return}
+}
