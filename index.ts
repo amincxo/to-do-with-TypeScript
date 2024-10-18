191bf2658f7375c0e6f9fcc6cd35eb1381a559a4
unknown <amincxo@gmail.com>
2024-10-19 02:56:20 +0330

    add type resposeMthod and create getList

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 enum State {
-    QUEUE="queue",
+    START="start",
     DONE="done",
     CANCEL="cancel",
     TOP_LIST="topList"
@@ -16,6 +16,9 @@ type createTodoDTO = {
     title: string;
     state : State;
 }
+type ResponseMehtod = {
+    message: string
+}
 
 interface ITodo {
     createTodo(todo: createTodoDTO): void
@@ -29,12 +32,8 @@ abstract class TodoRepository {
     constructor(){
         this.Todos = []
     }
-    public createTodo(todo: createTodoDTO): void {
-
-    }
-    public delete(id: number ): void {
-
-    }
+    public createTodo(todo: createTodoDTO): void {    }
+    public delete(id: number ): void {    }
     public getList(): void {    }
     public getByID(id: number) : void {   }
 }
@@ -44,12 +43,23 @@ class TodoController extends TodoRepository {
     constructor () {
         super()
     }
-    public createTodo(todo: createTodoDTO): any {
-        return {newTodo: "created"}
+    public createTodo(todo: createTodoDTO): ResponseMehtod {
+        const id = this.Todos.length
+        const newTodo:Todo = {
+            id,
+            title: todo.title,
+            state: todo.state
+        }
+        this.Todos.push(newTodo)
+        return {message: "created"}
     } 
+    public getList():Todo[] {
+        return this.Todos
+    }
 }
 
 const todo = new TodoController();
 
-console.log(todo.createTodo({title : " ", state: State.DONE }))
+console.log(todo.createTodo({title : " create todo list ", state: State.START }));
 
+console.log(todo.getList())
